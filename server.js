02d139a3b49cd5ccf2197ffb903638177be31a8e
AkaishiTakumi <115193708+AkaishiTakumi@users.noreply.github.com>
2025-10-29 17:00:02 +0900

    feat: 直前の単語を返すAPIを作成

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
+// 直前の単語を保持しておく
+let previousWord = "しりとり";
+
 // localhostにDenoのHTTPサーバーを展開
 Deno.serve(async (_req) => {
+    // パス名を取得する
+    // http://localhost:8000/hoge に接続した場合"/hoge"が取得できる
+    const pathname = new URL(_req.url).pathname;
+    console.log(`pathname: ${pathname}`);
+
+    // GET /shiritori: 直前の単語を返す
+    if (_req.method === "GET" && pathname === "/shiritori") {
+        return new Response(previousWord);
+    }
+
     // ./public以下のファイルを公開
     return serveDir(
         _req,
